2186ef9a3c8f6d5d2f7f328603896e79707eb474
DaisiSegun <daissegun096@gmail.com>
2023-10-07 01:45:32 +0100

    new code
    uah

--- front-end/my-app/src/setupProxy.js
+++ front-end/my-app/src/setupProxy.js
@@ -1,13 +1,11 @@
-// setupProxy.js
-
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
+module.exports = function(app) {
   app.use(
-    '/api', // Specify the endpoint you want to proxy
+    '/api', // Specify the API path you want to proxy
     createProxyMiddleware({
-      target: 'http://localhost:4000', // URL of the target server you want to proxy to
-      changeOrigin: true, // This ensures the host header is modified to match the target server's host
+      target: 'https://coral-app-xawf4.ondigitalocean.app', // The URL of your external API
+      changeOrigin: true,
     })
   );
 };
